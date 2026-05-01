07ac4bc88df3abb19a02661648d54b37a1da0153
tcastro <91tcastro@gmail.com>
2026-05-01 21:26:18 +0100

    chore(setup): use bundles in lib catalog + add modules dependencies to app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,19 +40,21 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.android)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    testImplementation(libs.bundles.testing)
+    androidTestImplementation(libs.bundles.uitesting)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(project(":core:ui"))
+    implementation(project(":data:breeds"))
+    implementation(project(":data:favourites"))
+    implementation(project(":domain:breeds"))
+    implementation(project(":domain:favourites"))
+    implementation(project(":feature:breeds"))
+    implementation(project(":feature:favourites"))
 }
